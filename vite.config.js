0b0d607967dff36eb42ce46695f5b5d01a9790c0
KiKiKi <chaika.design@gmail.com>
2022-12-06 14:33:09 +0900

    Add path alias configuration.
    cf. https://chaika.hatenablog.com/entry/2022/05/14/083000

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  root: "project",
-  base: "/",
-  publicDir: "public",
+  root: 'project',
+  base: '/',
+  publicDir: 'public',
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
   },
+  plugins: [
+    tsconfigPaths({
+      root: '../',
+    }),
+  ],
   server: {
     port: 3000,
     open: true,
